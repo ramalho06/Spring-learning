38fe56ef6e54f22da7b0fd16de5a2bb5f4b09796
daviramalho <daviramalho06@gmail.com>
2026-04-22 00:33:51 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
--- src/main/java/com/educandoweb/course/resources/UserResources.java
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -2,19 +2,35 @@ package com.educandoweb.course.resources;
 
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+    private final UserService service;
+    public UserResources(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "8399999999", "123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
