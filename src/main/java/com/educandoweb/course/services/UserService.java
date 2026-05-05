356637de28b52a5e44fbafa908e0ae88ece00b33
daviramalho <daviramalho06@gmail.com>
2026-05-05 02:35:08 -0300

    User Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -33,4 +33,25 @@ public class UserService {
     public void delete(Long id){
          repository.deleteById(id);
     }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        if(obj.getName() != null){
+            entity.setName(obj.getName());
+        }
+
+        if(obj.getEmail() != null){
+            entity.setEmail(obj.getEmail());
+        }
+
+        if (obj.getPhone() != null){
+            entity.setPhone(obj.getPhone());
+        }
+
+    }
 }
