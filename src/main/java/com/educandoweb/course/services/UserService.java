1ec28c5a65d16de4e8be56c1a916276373f052bb
daviramalho <daviramalho06@gmail.com>
2026-05-06 04:29:19 -0300

    Database Exception

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.aspectj.apache.bcel.generic.RET;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,7 +35,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-         repository.deleteById(id);
+         try {
+             repository.deleteById(id);
+         }catch (EmptyResultDataAccessException e){
+             throw new ResourceNotFoundException(id);
+         }catch (DataIntegrityViolationException e){
+             throw new DatabaseException(e.getMessage());
+         }
+
     }
 
     public User update(Long id, User obj){
